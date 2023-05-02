62307d292b04d58cc4934e630ff60a226157409e
黄金园 <huangjinyuan@huangjiuandembp.home>
2023-05-01 21:39:38 -0400

    no important thing change

--- test.js
+++ test.js
@@ -1,14 +1,29 @@
-async function fakeNetwork() {
+async function fakeNetwork(a) {
 
-    return new Promise(res => {
-      setTimeout(res, Math.random() * 800);
-    });
-  
-    // return new Promise((resolve, reject)=>{
-    //     console.log(resolve);
-    //     console.log(reject);
-    // })
-  }
+  console.log(arguments[0])
+  console.log(arguments.length)
+  return new Promise(res => {
+    setTimeout(res, Math.random() * 800);
+  });
+
+
+
+  // return new Promise((resolve, reject)=>{
+  //     console.log(resolve);
+  //     console.log(reject);
+  // })
+}
+
+const func =  (a)=>{
+  console.log('a', a);
+  console.log(arguments[0]);
+  return new Promise((res)=>{
+
+  })
+}
+
+
+func(44);
 
 //   function test (resolve, reject){
 //     console.log('resolve', resolve);
@@ -17,12 +32,30 @@ async function fakeNetwork() {
 
 //   test(5);
 //   test(5,6);
-fakeNetwork()
-.then(res => {
-    console.log('here');
-})
+// fakeNetwork()
+// .then(res => {
+//     console.log('here');
+// })
 // .catch(res=>{
 //     console.log('res catch', res);
 // })
+
+
+// let a = [4,5,66]
+// a.name = 'haha'
+// for (let key in a) {
+// console.log(typeof key);
+// }
+
+
+  
+// var a = ['A', 'B', 'C'];
+// a.forEach(function (element, index, array) {
+//     // element: 指向当前元素的值
+//     // index: 指向当前索引
+//     // array: 指向Array对象本身
+//     console.log(element + ', index = ' + index);
+//     console.log(array);
+// });
 
 
